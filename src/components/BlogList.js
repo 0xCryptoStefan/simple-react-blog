0402c929f979e3c735e981a64f29eccca8c2705f
0xStefan <blockchain.crypto.stefan@gmail.com>
2022-04-21 17:23:40 +0100

    Added data folder for JSON server

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -3,7 +3,7 @@ import React from "react";
 // Alternative method to passing props and creating variables is destructuring the props object in the argument
 // Standard call
 // export default function BlogList(props)
-export default function BlogList({ blogs, title }) {
+export default function BlogList({ blogs, title, handleDelete }) {
   // These would be used with a standard props argument
   //   const blogs = props.blogs;
   //   const title = props.title;
@@ -15,6 +15,7 @@ export default function BlogList({ blogs, title }) {
         <div className="blog-preview" key={blog.id}>
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}.</p>
+          <button onClick={() => handleDelete(blog.id)}>delete blog</button>
         </div>
       ))}
     </div>
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "../components/BlogList";
 
 export default function Home() {
@@ -13,19 +13,33 @@ export default function Home() {
     {
       title: "Ipsum",
       body: "The quick brown fox jumps over the lazy dog",
-      author: "Foxy Dog",
+      author: "Mario",
       id: 2,
     },
     {
       title: "Ipsum Finite",
       body: "The quick brown fox jumps over the lazy dog",
-      author: "Foxy Dog",
+      author: "Mario",
       id: 3,
     },
   ]);
+
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  };
+
+  useEffect(() => {
+    console.log("useEffect ran");
+  }, []); // add empty dependency array here if only want to run once, or add each state element to be monitored into the array e.g. [] or [data, additionalData, furtherData]
+
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All blogs" />
+      <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete} />
+      <BlogList
+        blogs={blogs.filter((blog) => blog.author === "Mario")}
+        title="Mario's blogs"
+      />
     </div>
   );
 }
