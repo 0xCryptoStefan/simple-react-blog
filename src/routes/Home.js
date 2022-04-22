3bba7f1e11e907c82135590d1ae7e2fffeb0a0a8
0xStefan <blockchain.crypto.stefan@gmail.com>
2022-04-22 23:50:52 +0100

    Part 20 completed

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,22 +1,19 @@
 import React from "react";
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 import BlogList from "../components/BlogList";
+import useFetch from "../hooks/useFetch";
 
 export default function Home() {
-  const [blogs, setBlogs] = useState(null);
-
-  useEffect(() => {
-    fetch("http://localhost:8000/blogs")
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        setBlogs(data);
-      });
-    console.log("useEffect ran");
-  }, []); // add empty dependency array here if only want to run once, or add each state element to be monitored into the array e.g. [] or [data, additionalData, furtherData]
+  const {
+    data: blogs,
+    isPending,
+    error,
+  } = useFetch("http://localhost:8000/blogs");
 
   return (
     <div className="home">
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All blogs" />}
       {/* <BlogList
         blogs={blogs.filter((blog) => blog.author === "Mario")}
